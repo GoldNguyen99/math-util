9908da945638dfd2d19432cbc816ac5ecb85bd80
goldnguyen99 <goldfpt4040@gmail.com>
2020-11-18 09:04:29 +0700

    add JUnit code & .jar(s)

--- src/nguyen/util/mathutil1.java
+++ src/nguyen/util/mathutil1.java
@@ -29,5 +29,30 @@ public class mathutil1 {
         //tham so >20, tham so <0
         System.out.println("expected: IlligalArgumentException" + MathUltility.getFactorial(-5));
     }
+  //Minh muon ket luan ham ngon, minh phai test het cac truong hop xai ham
+    //truong hop 5! )! -5! 21! 20! ... 
+    // minh phai thiet ke cac test case, tinh huong test de sau do minh verify 
+    //giua gia tri tra ve cua ham(actual) co khop voi expected cai minh du doan 
+    //ham chay phai tra ve
+    //va neu moi thu khop, tuc la actual == expected, minh moi dam gay: Ham ngon
+    //cach nay co nhuoc diem: nhin bang mat va so sanh ket qua cua tung cae
+    // tinh huong test 
+    //cach nay on~ nhung tim an sai sot do nhin bang mat nhieu tinh huong
+    // CACH NANG CAO
+    // cung nhin bang mat nhung chi nhin 2 mau xanh - do cho moi test case
+    //co nghia: neu ban xai ham voi 100 tham so, chay ham voi 100 tinh huong khac nhau. ban chir can nhin duy nhat 2 mau xanh va do
+    //xanh code on cho moi test case, moi tinh huong goi ham
+    // MOI CASE PHAI CO EXPECTED = ACTUAL thi moi xanh
+    // do moi case mau xanh chi co 1 thang do, ket luan ca dam do~
+    // ham dung cho 99 truong hop, expected = actual cho 99% tinh huong
     
+    //chi co 1 tinh huong sai, ko khop, ket luan luon, ham k on dinh
+    // ai dam xai khi k on dinh, on dinh va chinh xac cho moi truong hop
+    //chi can it nhat 1 cai khong on, do ca dam de dam bao rang ham phai chinh xac tinh huong
+    // Muon lam duoc dieu nay, can su phu giup cua cac thu vien ben ngoai JDK, goi la 
+    //unit test Framework
+    // Cac bo unit test noi tieng: JUnit, TstUnit,NUnit, xUnit,PHUnit,...
+    //bua nay minh hoc xanh do qua Unit
+    // Van voi muc tieu test ham co ngon khong, nhung chi nhin dung 2 au, k care chi tiet cac tinh huong chay
+ 
 }
---
+++ test/gold/util/test/MathUtilityTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gold.util.test;
+
+import static github.MathUltility.*;
+//chi co tu JDK 5? ham static goi ma hok can ten class cham 
+// do minh da choi tro khai bao san ten class va en ham static qua lenh 
+//import static
+//sau nay sai ham static cua  class nay ko can ten class cham, xai nhu C 
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class MathUtilityTest {
+    //viet ra nhung viec nhung tinh huong xai ham getFactorial()
+    //kiem tra xem actual == expected hay ko?? nhu truyen thong ben main()
+    //nhung o day xin so hon, show ra mau Xanh DO
+    //ngoai ra no con dung duoc cho viec CI - Continuous Intergration
+                                          //CD - Continuous Delivery
+    //                                     CT - Continuous Regression Test 
+    //                                      DEvOps - Development & Operation
+    //ngoai doanh nghiep dang xai
+    // a[[ lien tuc duoc kiem tra chat luong code, build ra file.jar.war
+    //day len 1 server trung gian - Staging Eviroment de cho nhom test
+    //san sang test ki
+    //sau do co the day thang len Production Server, may chay that o noi khach hang
+    //moi viec se bat dau ke tu luc Developer push code len Github
+    // moi thu cu the tu dong, ko can con nguoi can thiep.
+    @Test //tu khoa nay tuong ~~ bien cai ham nay thanh Public Static void mani()
+    //trong ham nay minh se show ra mau xanh do dua tren su khop nhau cua Expected vs. Actual qua ham assertEquals().. 
+    //ma thu vien cung cap
+    //assert..() du cac loai assert tren cuoc doi - assert co nghia la so sanh
+    //thay vi so sanh bang mat, ta nho ham so luon, thay~ ve mau gium ta.
+    public void getFactual_RunsWell_IfValidArgument(){
+        assertEquals(120, getFactorial(5));
+        assertEquals(24, getFactorial(4));
+        assertEquals(720, getFactorial(6));
+        assertEquals(0, getFactorial(0));
+        //vietsub: tinh dum tui 5! coi no co la 120 hem?
+        //no co thi bao mau xanh, ko thi bao do~ .
+        
+    }
+   
+    
+}
